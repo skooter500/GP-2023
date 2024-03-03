2a8d46b87b9af3c5a06afe6eeee4089b42bfe385
Bryan Duggan <skooter500@gmail.com>
2024-03-03 20:58:53 +0000

    wipppp

---
+++ Game Design Examples/bug.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	var offset = randf_range(-100, 100)
+	position.x += offset
+	position.y += 20
+	
+	pass # Replace with function body.
